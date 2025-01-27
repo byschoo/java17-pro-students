477f04d71f2c4f23c9d1eeebff0814b9ee2a86ea
127136013+byschoo@users.noreply.github.com <byschoo@outlook.com>
2025-01-27 02:04:19 +0100

    PROD: @Pattern deleted in adds was audited to Heroku deploy

--- src/main/java/com/byschoo/apirest_students/Controller/StudentController.java
+++ src/main/java/com/byschoo/apirest_students/Controller/StudentController.java
@@ -25,12 +25,12 @@ public class StudentController {
     private StudentService studentService;
 
     @PostMapping("/addStudent")
-    public Student addStudent(@RequestBody Student student) {
+    public Student addStudent(@RequestBody @Valid Student student) {
     return studentService.saveStudent(student);
     }
 
     @PostMapping("/addStudents")
-    public List<Student> addStudents(@RequestBody List<Student> students) {
+    public List<Student> addStudents(@RequestBody @Valid List<Student> students) {
     return studentService.saveStudents(students);
     }
 
