12189432a79eb57610c454552bc5e2ef2eeae582
127136013+byschoo@users.noreply.github.com <byschoo@outlook.com>
2025-01-27 04:16:30 +0100

    PROD: Releasing @NotBlank to Heroku deploy

--- src/main/java/com/byschoo/apirest_students/Entity/Student.java
+++ src/main/java/com/byschoo/apirest_students/Entity/Student.java
@@ -8,8 +8,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 /*import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;*/
+import jakarta.validation.constraints.Min;*/
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 /*import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;*/
@@ -29,8 +29,8 @@ public class Student {
    private Long id;
 
    @Column(name = "name", nullable = false)
-   /*@NotBlank(message = "El nombre es obligatorio")
-   @Pattern(regexp = "^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ\\s]+$", message = "El nombre solo puede contener letras latinas y espacios")
+   @NotBlank(message = "El nombre es obligatorio")
+   /*@Pattern(regexp = "^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ\\s]+$", message = "El nombre solo puede contener letras latinas y espacios")
    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres") */
    private String name;
 
