a7bd2ba54d0dca480459681db1f92324c9dd4db2
127136013+byschoo@users.noreply.github.com <byschoo@outlook.com>
2025-01-27 04:23:39 +0100

    PROD: Releasing @Pattern @Size to Heroku deploy

--- src/main/java/com/byschoo/apirest_students/Entity/Student.java
+++ src/main/java/com/byschoo/apirest_students/Entity/Student.java
@@ -11,8 +11,8 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-/*import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;*/
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -30,8 +30,8 @@ public class Student {
 
    @Column(name = "name", nullable = false)
    @NotBlank(message = "El nombre es obligatorio")
-   /*@Pattern(regexp = "^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ\\s]+$", message = "El nombre solo puede contener letras latinas y espacios")
-   @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres") */
+   @Pattern(regexp = "^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ\\s]+$", message = "El nombre solo puede contener letras latinas y espacios")
+   @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
    private String name;
 
    @Column(name = "age", nullable = false)
