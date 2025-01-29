97a012c06241dae218c0c19c59156ebce2ddd2bf
127136013+byschoo@users.noreply.github.com <byschoo@outlook.com>
2025-01-30 00:43:36 +0100

    PROD: Added CrudRepository Repository to Heroku deploy

--- src/main/java/com/byschoo/apirest_students/Repository/IStudentRepository.java
+++ src/main/java/com/byschoo/apirest_students/Repository/IStudentRepository.java
@@ -2,11 +2,11 @@ package com.byschoo.apirest_students.Repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.byschoo.apirest_students.Entity.Student;
 
 
-public interface IStudentRepository extends JpaRepository<Student, Long> {
+public interface IStudentRepository extends CrudRepository<Student, Long> {
    List<Student> findByStudentName(String name);
 }
