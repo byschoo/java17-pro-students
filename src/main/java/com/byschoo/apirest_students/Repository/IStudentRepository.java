fa2d75f4159cca9d086be96a44c5f1ed4b2746e4
127136013+byschoo@users.noreply.github.com <byschoo@outlook.com>
2025-01-30 00:28:03 +0100

    PROD: deleted CrudRepository Repository to Heroku deploy

--- src/main/java/com/byschoo/apirest_students/Repository/IStudentRepository.java
+++ src/main/java/com/byschoo/apirest_students/Repository/IStudentRepository.java
@@ -2,11 +2,11 @@ package com.byschoo.apirest_students.Repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.byschoo.apirest_students.Entity.Student;
 
 
-public interface IStudentRepository extends CrudRepository<Student, Long> {
+public interface IStudentRepository extends JpaRepository<Student, Long> {
    List<Student> findByStudentName(String name);
 }
