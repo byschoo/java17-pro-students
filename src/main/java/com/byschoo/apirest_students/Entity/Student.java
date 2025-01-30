2d72f8832daf27661647de5a43fb34ae2759486b
127136013+byschoo@users.noreply.github.com <byschoo@outlook.com>
2025-01-30 02:01:50 +0100

    PROD: @Pattern Entity to Heroku deploy

--- src/main/java/com/byschoo/apirest_students/Entity/Student.java
+++ src/main/java/com/byschoo/apirest_students/Entity/Student.java
@@ -11,6 +11,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,7 +30,7 @@ public class Student {
 
    @Column(name = "name", nullable = false)
    @NotBlank(message = "El nombre es obligatorio")
-  // @Pattern(regexp = "^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ\\s]+$", message = "El nombre solo puede contener letras latinas y espacios")
+   @Pattern(regexp = "^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ\\s]+$", message = "El nombre solo puede contener letras latinas y espacios")
    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
    private String name;
 
--- src/main/java/com/byschoo/apirest_students/Repository/IStudentRepository.java
+++ src/main/java/com/byschoo/apirest_students/Repository/IStudentRepository.java
@@ -1,5 +1,6 @@
 package com.byschoo.apirest_students.Repository;
 
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.byschoo.apirest_students.Entity.Student;
 
 
-public interface IStudentRepository extends JpaRepository<Student, Long> {
-   List<Student> findByName(String name);
+public interface IStudentRepository extends JpaRepository<Student,Long> {
+    List<Student> findByName(String name);
 }
